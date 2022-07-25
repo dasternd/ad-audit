430968fe49df28479e4efc63fbf6d5f94812626c
dasternd <danil@stepanov.name>
2022-07-25 16:52:01 +0300

    Add PS scripts

---
+++ PowerShell/Get-InfoAD.ps1
@@ -0,0 +1,11 @@
+﻿#Получение информации о домене
+Get-ADDomain > c:\audit\ad\addomain.txt
+
+#Получение информации обо всех контроллерах домена
+Get-ADDomainController -filter * > c:\audit\ad\addomaincontroller.txt
+
+#Получение информации о лесе
+Get-ADForest > c:\audit\ad\adForest.txt
+
+#Получение информации о DNS
+Get-DnsServer > c:\audit\ad\dnsserver.txt
---
+++ PowerShell/Get-InfoServer.ps1
@@ -0,0 +1,23 @@
+﻿#Сбор информации о сервере
+Get-ComputerInfo > c:\audit\computerinfo.txt
+
+#Сбор информации о запущенных процессах
+Get-Process > c:\audit\process.txt
+
+#Сбор информации об установленных панелей управления
+Get-ControlPanelItem > c:\audit\controlpanel.txt
+
+#Сбор об установленных исправлениях
+Get-HotFix > c:\audit\hotfix.txt
+
+#Получает диски в текущем сеансе
+Get-PSDrive > c:\audit\drive.txt
+
+#Получения служб на сервере
+Get-Service > c:\audit\service.txt
+
+#Получение установленные роли
+Get-WindowsFeature > c:\audit\windowsfeature.txt
+
+#Получение установленного ПО
+Get-WmiObject -Class Win32_Product > c:\audit\software.txt
