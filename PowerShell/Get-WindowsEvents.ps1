9e5c5a9ac1507f2059c5e088d89eb0b44c45dc86
dasternd <danil@stepanov.name>
2022-07-25 14:30:53 +0300

    Add PS script

---
+++ PowerShell/Get-WindowsEvents.ps1
@@ -0,0 +1,31 @@
+﻿#
+#  This script exports consolidated and filtered event logs to CSV
+#  Author: Danil Stepanov, msware.ru (c) 2022
+#
+
+Set-Variable -Name EventAgeDays -Value 7     #we will take events for the latest 7 days
+Set-Variable -Name CompArr -Value @("DC1", "DC2")   # replace it with your server names
+Set-Variable -Name LogNames -Value @("Application", "System", "DFS Replication", "Directory Service", "DNS Server")  # Checking app and system logs
+Set-Variable -Name EventTypes -Value @("Error", "Warning")  # Loading only Errors and Warnings
+Set-Variable -Name ExportFolder -Value "C:\Temp\"
+
+
+$el_c = @()   #consolidated error log
+$now=get-date
+$startdate=$now.adddays(-$EventAgeDays)
+$ExportFile=$ExportFolder + "el" + $now.ToString("yyyy-MM-dd--hh-mm-ss") + ".csv"  # we cannot use standard delimiteds like ":"
+
+foreach($comp in $CompArr)
+{
+  foreach($log in $LogNames)
+  {
+    Write-Host Processing $comp\$log
+    $el = get-eventlog -ComputerName $comp -log $log -After $startdate -EntryType $EventTypes
+    $el_c += $el  #consolidating
+  }
+}
+$el_sorted = $el_c | Sort-Object TimeGenerated    #sort by time
+Write-Host Exporting to $ExportFile
+# $el_sorted | Select EntryType, TimeGenerated, Source, EventID, MachineName | Export-CSV $ExportFile -NoTypeInfo  #EXPORT
+$el_sorted | Export-CSV $ExportFile -NoTypeInfo  #EXPORT
+Write-Host Done!
