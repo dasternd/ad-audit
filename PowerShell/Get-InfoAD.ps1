213ea23d920b933a45da0308598210551bc7e0db
dasternd <danil@stepanov.name>
2022-07-26 16:21:26 +0300

    Edit PS script

--- PowerShell/Get-InfoAD.ps1
+++ PowerShell/Get-InfoAD.ps1
@@ -1,11 +1,17 @@
-﻿#Получение информации о домене
+﻿Write-Host Exporting info about Domain
+#Получение информации о домене
 Get-ADDomain > c:\audit\ad\ADDomain.txt
 
+Write-Host Exporting info about Domain Controller
 #Получение информации обо всех контроллерах домена
 Get-ADDomainController -filter * > c:\audit\ad\ADDomainController.txt
 
+Write-Host Exporting info about Forest
 #Получение информации о лесе
 Get-ADForest > c:\audit\ad\ADForest.txt
 
+Write-Host Exporting info about DNS
 #Получение информации о DNS
 Get-DnsServer > c:\audit\ad\DNSSserver.txt
+
+Write-Host Done!
--- PowerShell/Get-InfoServer.ps1
+++ PowerShell/Get-InfoServer.ps1
@@ -1,23 +1,33 @@
-﻿#Сбор информации о сервере
+﻿Write-Host Exporting info about Computer Info
+#Сбор информации о сервере
 Get-ComputerInfo > c:\audit\ComputerInfo.txt
 
+Write-Host Exporting info about Precesses
 #Сбор информации о запущенных процессах
 Get-Process > c:\audit\Process.txt
 
+Write-Host Exporting info about Control Panel
 #Сбор информации об установленных панелей управления
 Get-ControlPanelItem > c:\audit\ControlPanel.txt
 
+Write-Host Exporting info about Hotfix
 #Сбор об установленных исправлениях
 Get-HotFix > c:\audit\Hotfix.txt
 
+Write-Host Exporting info about Drive
 #Получает диски в текущем сеансе
 Get-PSDrive > c:\audit\Drive.txt
 
+Write-Host Exporting info about Service
 #Получения служб на сервере
 Get-Service > c:\audit\Service.txt
 
+Write-Host Exporting info about Windows Feature
 #Получение установленные роли
 Get-WindowsFeature > c:\audit\WindowsFeature.txt
 
+Write-Host Exporting info about Software
 #Получение установленного ПО
 Get-WmiObject -Class Win32_Product > c:\audit\Software.txt
+
+Write-Host Done!
