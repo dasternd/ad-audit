10e8256813c2455ba00b9202c05b5e40e3ebe843
dasternd <danil@stepanov.name>
2022-08-01 12:43:05 +0300

    Edit PS script

--- PowerShell/Prerequisite.ps1
+++ PowerShell/Prerequisite.ps1
@@ -3,6 +3,24 @@
 #  Author: Danil Stepanov, msware.ru (c) 2022
 #
 
+# адрес каталога проекта, где будет размещены все входные и выходные файлы
+$pathProject = Get-Location 
+$pathProject = $pathProject.Path + "\"
+# адрес размещения лог-файла
+$pathLogFile = $pathProject + "Prerequisite-" + (Get-Date).ToString('yyyy_MM_dd_HH_mm') + ".log" 
+
+# ФУНКЦИЯ ЛОГИРОВАНИЯ СОБЫТИЙ
+function WriteLog
+{
+    Param ([string]$LogString)
+    $LogFile = $pathLogFile
+    $DateTime = "[{0:dd/MM/yy} {0:HH:mm:ss}]" -f (Get-Date)
+    $LogMessage = "$Datetime $LogString"
+    Add-content $LogFile -value $LogMessage
+}
+
+Write-Host "Prerequisite for audit Active Directory"
+
 Write-Host Forest Mode
 $ADForest = Get-ADForest
 $forestMode = $ADForest.ForestMode
@@ -41,15 +59,26 @@ $SchemaMaster
 
 Write-Host
 Write-Host All Domain Controllers
-$domControllers = Get-ADDomainController -filter * | Select-Object HostName 
+WriteLog "List Domain Controllers"
+$domControllers = Get-ADDomainController -filter * | Select-Object HostName
+$totalDC = $domControllers.Count
 for ($i = 0; $i -lt $domControllers.Count; $i++) {
+    WriteLog $domController.HostName PS version $PSVer.PSVersion
     $domControllers[$i].HostName
 } 
+WriteLog "[Info] Total Domain Controllers $totalDC"
 
+WriteLog "Version PowerShell"
 foreach ($domController in $domControllers) {
     $PSVer = Invoke-Command -ComputerName $domController.HostName -ScriptBlock { $PSVersionTable }
-    Write-Host
-    Write-Host $domController.HostName PS version $PSVer.PSVersion 
+    try {
+        Write-Host
+        WriteLog $domController.HostName PS version $PSVer.PSVersion
+        Write-Host $domController.HostName PS version $PSVer.PSVersion 
+    }
+    catch {
+        Write-Host
+        Write-Host $Error
+        WriteLog $Error
+    }
 } 
-
- 
